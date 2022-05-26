f9437f1ffaa8e902cb1fd83108d5d61b2548e05a
nolansf <72474283+nolansf@users.noreply.github.com>
2022-05-26 17:27:47 -0400

    Commit

--- scripts/ideas.js
+++ scripts/ideas.js
@@ -1,1 +1,13 @@
 
+window.onload = () => {
+    setInterval(updateClock, 1000);
+}
+
+function updateClock(){
+    const current = new Date();
+    const clock = document.getElementById("clock");
+    const h = current.getHours();
+    const m = current.getMinutes();
+    const s = current.getSeconds();
+    clock.innerHTML = `${h}:${m}:${s}`;
+}
