0cb70b16ea8012cbd509f2953bfe6b4ef53c6fa8
nolansf <72474283+nolansf@users.noreply.github.com>
2022-05-31 22:04:25 -0400

    Functional

--- scripts/learning.js
+++ scripts/learning.js
@@ -18,8 +18,7 @@ const app = {
 
         fetch(url)
         .then(resp => {
-            if (!resp.ok) throw new Error(resp.statusText);
-            return resp.json();
+            if (resp.ok) return resp.json();
         })
         .then(data => {
             app.showWeather(data);
@@ -43,7 +42,106 @@ const app = {
     },
     showWeather: (resp) => {
         console.log(resp);
+        let main = document.querySelector('.sample-card-current');
+        main.innerHTML = `<h5>Current's Forecast</h5>
+        <div class="sample-card-banner">
+            <div>
+                <p class="sample-card-text big">${resp.current.feels_like.toFixed(0)}&deg;</p>
+                <p class="sample-card-text">Feels Like</p>
+            </div>
+            <div>
+                <img src="https://openweathermap.org/img/wn/${resp.current.weather[0].icon}@4x.png" alt="Weather description" class="card-img">
+                <p class="sample-card-img-desciption">${resp.current.weather[0].main}</p>    
+            </div>
+        </div>
+        
+        <div class="sample-card-body">
+            <div class="sample-card-table">
+                <div class="sample-card-col">
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">High / Low</p>
+                        <p class="sample-card-text">${resp.daily[0].temp.max.toFixed(0)} / ${resp.daily[0].temp.min.toFixed(0)}</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Humidity</p>
+                        <p class="sample-card-text">${resp.current.humidity}</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Pressure</p>
+                        <p class="sample-card-text">${(resp.current.pressure * 0.0145038).toFixed(2)} in</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Visibility</p>
+                        <p class="sample-card-text">${resp.current.visibility / 1000}km</p>
+                    </div>
+                </div>
+                <div class="sample-card-col">
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Wind</p>
+                        <p class="sample-card-text">${resp.current.wind_speed.toFixed(0)}mph ${resp.current.wind_deg}&deg;</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Dew Point</p>
+                        <p class="sample-card-text">${resp.current.dew_point}</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">UV Index</p>
+                        <p class="sample-card-text">${resp.current.uvi}</p>
+                    </div>
+                    <div class="sample-card-row">
+                        <p class="sample-card-text">Sunrise</p>
+                        <p class="sample-card-text">${new Date(resp.current.sunrise * 1000).toTimeString()}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+        // counting to 7 for no reason
+        let hour = document.querySelector('.hour');
+        let hourhtml =  `
+        <div class="sample-card-hour">
+            <p class="sample-card-title now">Now</p>
+            <p class="sample-card-degree">${resp.hourly[0].temp.toFixed(0)}&deg;</p>
+            <img class="sample-card-img" src="https://openweathermap.org/img/wn/${resp.hourly[0].weather[0].icon}@2x.png">
+            <p class="sample-card-img-desciption">${resp.hourly[0].weather[0].main}</p>
+        </div>`;
+
+        for (var i = 1; i < 5; i++) {
+            hourhtml += `
+            <div class="sample-card-hour">
+                <p class="sample-card-title">${new Date(resp.hourly[i].dt * 1000).getHours()}</p>
+                <p class="sample-card-degree">${resp.hourly[i].temp.toFixed(0)}&deg;</p>
+                <img class="sample-card-img" src="https://openweathermap.org/img/wn/${resp.hourly[i].weather[0].icon}@2x.png">
+                <p class="sample-card-img-desciption">${resp.hourly[i].weather[0].main}</p>
+            </div>`;
+        }
+        hour.innerHTML = hourhtml;
+
+
+        let day = document.querySelector('.day');
+        let dayhtml = `
+        <div class="sample-card-hour">
+            <p class="sample-card-title now">Today</p>
+            <p class="sample-card-degree-high">${resp.daily[0].temp.max.toFixed(0)}&deg;</p>
+            <p class="sample-card-degree-low">${resp.daily[0].temp.min.toFixed(0)}&deg;</p>
+            <img class="sample-card-img" src="https://openweathermap.org/img/wn/${resp.daily[0].weather[0].icon}@2x.png">
+            <p class="sample-card-img-desciption">${resp.daily[0].weather[0].main}</p>
+        </div>`;
+
+        for (var i = 1; i < 5; i++) {
+            dayhtml += `
+            <div class="sample-card-hour">
+                <p class="sample-card-title">${new Date(resp.daily[i].dt * 1000).toDateString()}</p>
+                <p class="sample-card-degree-high">${resp.daily[i].temp.max.toFixed(0)}&deg;</p>
+                <p class="sample-card-degree-low">${resp.daily[i].temp.min.toFixed(0)}&deg;</p>
+                <img class="sample-card-img" src="https://openweathermap.org/img/wn/${resp.daily[i].weather[0].icon}@2x.png">
+                <p class="sample-card-img-desciption">${resp.daily[i].weather[0].main}</p>
+            </div>`;
+        }
+        day.innerHTML = dayhtml;
+        
     },
 };
+
 
 app.init();
